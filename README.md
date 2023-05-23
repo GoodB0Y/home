fea00f71af6dbf108d440a6526298842e52413d7
igordolinik <IDolinik@beeline.ru>
2023-05-23 22:33:51 +0300

    first in feature

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # home
-1. something text
+1. something text 1
+2. something text 2
+3. something text 3
---
+++ utils.js
@@ -0,0 +1,18 @@
+/**
+ * Тестовая функция
+ * @param a
+ * @param b
+ */
+const someFunc = (a, b) => {
+    console.log('something' + a + b);
+};
+
+/**
+ * Тестовая функция 2
+ * @param a
+ * @param b
+ * @param c
+ */
+const addNumber = (a, b, c) => {
+    return  a + b + c;
+};
